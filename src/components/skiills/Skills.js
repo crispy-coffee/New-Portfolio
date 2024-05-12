ddbde6b22fcf9c45e3f66e38e1f62030abd09b2f
Crispy-coffee <devverma712@gmail.com>
2024-05-12 20:02:17 +0530

    Updated

--- src/components/skiills/Skills.js
+++ src/components/skiills/Skills.js
@@ -10,7 +10,7 @@ const Skills = () => {
       <h2 className="section__title">Skills</h2>
       <span className="section__subtitle">My technical level</span>
 
-      <div className="skills__container container grid" style={{display:'flex'}}>
+      <div className="skills__container container grid">
         <Frontend/>
         <Backend/>
         <OtherSkills />
