2bdaffd7774009fc03bf4bd792198c007cc62f96
Crispy-coffee <devverma712@gmail.com>
2024-05-12 19:38:33 +0530

    Update Resume

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './about.css';
 import AboutImg from '../../assets/about.jpeg';
-import CV from '../../assets/resume.pdf';
+import CV from '../../assets/DevenderVerma.pdf';
 import Info from './Info';
 
 
